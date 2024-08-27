1a8013bfc99117abc7acea2851b0f0868f7263f2
PetertheRedCedar <ftpb2024.1776@gmail.com>
2024-08-27 12:57:30 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install curl -y
+curl -fsSL https://ollama.com/install.sh | sh
+chmod +x ryfai_linux
+./ryfai_linux
---
+++ install_fedora.sh
@@ -0,0 +1,4 @@
+sudo dnf install curl -y
+curl https://ollama.com/install.sh | sh
+chmod +x ryfai_linux
+./ryfai_linux
